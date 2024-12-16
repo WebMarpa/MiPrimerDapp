04c9933712f32bbee121c561616bc768fa00cfad
WebMarpa <mario_amilcar_parodi@hotmail.com>
2024-12-16 06:54:46 +0000

    delete folder github workflow an coment # yarn lint-staged --verbose

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Link from "next/link";
 import { hardhat } from "viem/chains";
-import { CurrencyDollarIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { HeartIcon } from "@heroicons/react/24/outline";
 import { SwitchTheme } from "~~/components/SwitchTheme";
 import { BuidlGuidlLogo } from "~~/components/assets/BuidlGuidlLogo";
